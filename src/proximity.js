ba23fe6e8a810bc7ce0cbc3c22b50a835ee19773
Daniel Friesen <github@nadir-seen-fire.com>
2009-12-10 18:26:51 -0800

    Finish porting proximity

--- src/proximity.js
+++ src/proximity.js
@@ -104,44 +104,23 @@ Proximity.dijkstraShortestPath = function(graph, id1, id2, heuristic, directed) 
 		}
 	}
 	
-	var q = 
-	
+	var q = [{ cost1: 0, v1: start, path: []}] // Array of [cost, path_head, path_rest].
+	var visited = {}; // Visited vertices; Python used a Set() of strings
+	for(;;) {
+		var qq = q.pop();
+		visited[qq.v1] = true;
+		if ( qq.v1 === end ) {
+			var r = flatten(qq.path).slice().reverse()
+			r.push(qq.v1);
+			return r;
+		}
+		var path = [qq.v1, qq.path];
+		for ( var g in G[qq.v1] ) {
+			if ( !visited[g.v2] )
+				q.push([qq.cost1 + g.cost2, g.v2, qq.path]);
+		}
+	}
 };
-
-
-#--- DIJKSTRA SHORTEST PATH --------------------------------------------------------------------------
-
-def dijkstra_shortest_path(graph, id1, id2, heuristic=None, directed=False):
-
-    """ Dijkstra algorithm for finding shortest paths.
-    
-    Connelly Barnes, http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/119466
-    Raises an IndexError between nodes on unconnected graphs.
-    
-    """
-    
-    G = adjacency(graph, directed=directed, heuristic=heuristic)
-    start = id1
-    end = id2
-    
-    # Flatten linked list of form [0,[1,[2,[]]]]
-    def flatten(L):       
-        while len(L) > 0:
-            yield L[0]
-            L = L[1]
-
-    q = [(0, start, ())]  # Heap of (cost, path_head, path_rest).
-    visited = Set()       # Visited vertices.
-    while True:
-        (cost1, v1, path) = heapq.heappop(q)
-        if v1 not in visited:
-            visited.add(v1)
-        if v1 == end:
-            return list(flatten(path))[::-1] + [v1]
-        path = (v1, path)
-        for (v2, cost2) in G[v1].iteritems():
-            if v2 not in visited:
-                heapq.heappush(q, (cost1 + cost2, v2, path))
 
 /**
  * Betweenness centrality for nodes in the graph.
@@ -251,50 +230,73 @@ Proximity.brandesBetweennessCentrality = function(graph, normalized, directed) {
  * The algorithm is adapted from NetworkX, Aric Hagberg (hagberg@lanl.gov):
  * https://networkx.lanl.gov/attachment/ticket/119/eigenvector_centrality.py
  */
-Proximity.eigenvectorCentrality = function(graph, ... ) {
+Proximity.eigenvectorCentrality = function(o) {
+	if ( !o.graph )
+		throw new Error("eigenvectorCentrality object args does not contain a required graph.");
+	if ( o.normalized === undefined )
+		o.normalized = true;
+	if ( o.reversed === undefined )
+		o.reversed = true;
+	o.rating = {};
+	o.iterations = o.iterations || 100;
+	if ( o.tolerance === undefined )
+		o.tolerance = 0.0001;
 	
+	var count = CanvasNode.util.count(this.graph.nodes);
+	var W = Proximity.adjacency({ graph: o.graph, directed: true, reversed: o.reversed });
+	
+	function _normalize(x) {
+		var s = CanvasNode.util.sum(CanvasNode.util.values(x));
+		if ( s != 0 )
+			s = 1.0 / s;
+		for ( k in x )
+			x[k] = x[k] * s;
+	}
+	
+	var x = o.start;
+	if ( !x ) {
+		x = {};
+		for ( var n in graph.nodes )
+			x[n] = Math.random();
+	}
+	_normalize(x);
+	
+	// Power method: y = Ax multiplication.
+	for ( var i = 0; i < o.iterations; i++ ) {
+		var x0 = x;
+		var x = {};
+		for ( var k in x0 )
+			x[k] = 0;
+		for ( var n in x ) {
+			for ( var nbr in W[n] ) {
+				var r = n in rating ? rating[n] : 1;
+				x[n] += 0.01 + x0[nbr] * W[n][nbr] * r;
+			}
+		}
+		_normalize(x);
+		var e;
+		for ( var n in x )
+			e += Math.abs(x[n]-x0[n]);
+		if ( e < count * o.tolerance ) {
+			if ( o.normalized ) {
+				// Normalize between 0 and 1
+				var m = Math.max.apply(Math, CanvasNode.util.values(x));
+				if ( m == 0 )
+					m = 1;
+				x2 = {};
+				for ( var id in x )
+					x[id] = x[id]/m;
+				x = x2;
+			}
+			return x;
+		}
+	}
+	
+	if ( window.console )
+		console.warn("node weight is 0 because eigenvector_centrality() did not converge.");
+	var x = {};
+	for ( var n in graph.nodes )
+		x[n] = 0;
+	return x;
 };
 
-#--- EIGENVECTOR CENTRALITY --------------------------------------------------------------------------
-
-class NoConvergenceError(Exception): pass
-
-def eigenvector_centrality(graph, normalized=True, reversed=True, rating={},
-                           start=None, iterations=100, tolerance=0.0001):
-
-    G = graph.keys()     
-    W = adjacency (graph, directed=True, reversed=reversed)
-
-    def _normalize(x):
-        s = sum(x.values())
-        if s != 0: s = 1.0 / s
-        for k in x: 
-            x[k] *= s
-    
-    x = start
-    if x is None:
-        x = dict([(n, random()) for n in G])
-    _normalize(x)
-
-    # Power method: y = Ax multiplication.
-    for i in range(iterations):
-        x0 = x
-        x = dict.fromkeys(x0.keys(), 0)
-        for n in x:
-            for nbr in W[n]:
-                r = 1
-                if rating.has_key(n): r = rating[n]
-                x[n] += 0.01 + x0[nbr] * W[n][nbr] * r
-        _normalize(x)          
-        e = sum([abs(x[n]-x0[n]) for n in x])
-        if e < len(graph.nodes) * tolerance:
-            if normalized:
-                # Normalize between 0.0 and 1.0.
-                m = max(x.values())
-                if m == 0: m = 1
-                x = dict([(id, w/m) for id, w in x.iteritems()])
-            return x
-
-    #raise NoConvergenceError
-    warn("node weight is 0 because eigenvector_centrality() did not converge.", Warning)
-    return dict([(n, 0) for n in G])
